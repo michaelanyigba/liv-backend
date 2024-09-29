b4f6dd118efbf0b8926c4424d89e024c0f81a06e
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 15:51:12 -0700

    changes made in socket io

--- socket/Socket.js
+++ socket/Socket.js
@@ -9,7 +9,7 @@ app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://localhost:3001" ],
+        origin: ["https://livfrontend.onrender.com" ],
         methods: ["GET", "POST"]
     }
 });
