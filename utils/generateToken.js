2b5aaea8b293ebc53c9741a511a5ea0e1ea4d48b
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-29 23:39:35 -0700

    cookies

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,9 +7,23 @@ const generateTokenAndSetCookie = (userId, res)=>{
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true, // prevent XSS attacks
-        sameSite: "strict", // prevent CSRF attack
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "none", // prevent CSRF attack
+        secure: true
     })
 }
-
 export default generateTokenAndSetCookie
+
+// export default generateTokenAndSetCookie
+// const generateTokenAndSetCookie = (userId, res)=>{
+//     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+//         expiresIn: "15d"
+//     });
+//     res.cookie("jwt", token, {
+//         maxAge: 15 * 24 * 60 * 60 * 1000,
+//         httpOnly: true, // prevent XSS attacks
+//         sameSite: "no", // prevent CSRF attack
+//         secure: process.env.NODE_ENV !== "production"
+//     })
+// }
+
+// 
